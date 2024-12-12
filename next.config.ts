cf1ef0cb81c18f59e809b21b6b07a56332433b02
mckeekaylie <59926734+mckeekaylie@users.noreply.github.com>
2024-12-12 14:16:45 -0500

    revert accidental config changes

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    formats: ["image/avif", "image/webp"],
-    loader: "default",
-    minimumCacheTTL: 60,
-  },
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
-  },
-  webpack: (config) => {
-    config.optimization.minimize = true;
-
-    return config;
-  },
 };
 
 export default nextConfig;
