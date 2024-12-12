c703a82686b99332809251c6059553d692edef8e
mckeekaylie <59926734+mckeekaylie@users.noreply.github.com>
2024-12-12 14:16:04 -0500

    convert loading galaxy image to local image

--- src/app/components/loading.tsx
+++ src/app/components/loading.tsx
@@ -1,16 +1,13 @@
 import styles from "../styles/loading-error.module.scss";
 
+import galaxy from "../../../public/images/bkgd-galaxy.webp";
+
 import Image from "next/image";
 
 export default function LoadingScreen() {
   return (
     <div className={styles.loadingScreen} data-testid="loadingScreen">
-      <Image
-        src="/images/bkgd-galaxy.webp"
-        alt="Galaxy background by Freepik"
-        fill
-        priority
-      />
+      <Image src={galaxy} alt="Galaxy background by Freepik" fill priority />
       <Image
         className={styles.loaderSvg}
         src="/images/icons/starfighter.svg"
